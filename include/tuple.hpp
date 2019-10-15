f1389517114b44efb0a2f56a84d2330de5e0fbba
NathanSWard <nathansward@comcast.net>
2019-10-14 23:00:49 -0600

    fix tuple for_each

--- include/tuple.hpp
+++ include/tuple.hpp
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include "type_traits.hpp"
+
 #include <tuple>
 
 namespace extra {
@@ -12,7 +14,7 @@ noexcept(noexcept((std::forward<Fn>(fn)(std::get<Is>(std::forward<Tpl>(tpl))), .
     (std::forward<Fn>(fn)(std::get<Is>(std::forward<Tpl>(tpl))), ...);
 }
 
-template<class Fn, class Tpl, class Seq = std::make_index_sequence<std::tuple_size_v<Tpl> - 1>>
+template<class Fn, class Tpl, class Seq = std::make_index_sequence<std::tuple_size_v<remove_cvref_t<Tpl>>>>
 void for_each(Fn&& fn, Tpl&& tpl) 
 noexcept(noexcept(_for_each_impl(std::forward<Fn>(fn), std::forward<Tpl>(tpl), Seq{}))) {
     _for_each_impl(std::forward<Fn>(fn), std::forward<Tpl>(tpl), Seq{}); 
