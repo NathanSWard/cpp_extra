2205b61f7fa93f5bda78de8f230c1123b1df58d8
NathanSWard <nathansward@comcast.net>
2019-10-15 00:45:44 -0600

    zip implementation

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -4,6 +4,8 @@
 
 #include <algorithm>
 #include <iterator>
+#include <tuple>
+#include <utility>
 
 namespace extra {
 
@@ -42,6 +44,68 @@ constexpr T min_unused(ExPo&& exec, ForwardIt first, ForwardIt last, T value = {
             last = p;
     }
     return value;
+}
+
+// zip
+template <typename... T>
+class _zip_impl {
+public:
+    class iterator : std::iterator<std::forward_iterator_tag,
+        std::tuple<decltype(*std::begin(std::declval<T>()))...>> {
+    private:
+        std::tuple<decltype(std::begin(std::declval<T>()))...> iters_;
+
+        template<std::size_t... I>
+        constexpr auto deref(std::index_sequence<I...>) const {
+            return typename iterator::value_type{*std::get<I>(iters_)...};
+        }
+
+        template<std::size_t... I>
+        constexpr void increment(std::index_sequence<I...>) {
+            ((void)++std::get<I>(iters_), ...);
+        }
+
+    public:
+        constexpr explicit iterator(decltype(iters_) iters) 
+            : iters_{std::move(iters)} 
+        {}
+
+        constexpr iterator& operator++() {
+            increment(std::index_sequence_for<T...>{});
+            return *this;
+        }
+
+        constexpr iterator operator++(int) {
+            auto saved{*this};
+            increment(std::index_sequence_for<T...>{});
+            return saved;
+        }
+
+        constexpr bool operator!=(const iterator& other) const {
+            return iters_ != other.iters_;
+        }
+
+        constexpr auto operator*() const { 
+            return deref(std::index_sequence_for<T...>{}); 
+        }
+    };
+
+    constexpr explicit _zip_impl(T&... seqs)
+        : begin_{std::make_tuple(std::begin(seqs)...)}
+        , end_{std::make_tuple(std::end(seqs)...)} 
+    {}
+
+    iterator begin() const { return begin_; }
+    iterator end() const { return end_; }
+
+private:
+    iterator begin_;
+    iterator end_;
+};
+
+template <typename... T>
+constexpr auto zip(T&&... seqs) {
+    return _zip_impl<T...>{std::forward<T>(seqs)...};
 }
 
 }
