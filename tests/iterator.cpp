748042db86914766cb739a9d914f5315e31739da
NathanSWard <nathansward@comcast.net>
2019-10-15 14:16:42 -0600

    add zip() test

--- tests/iterator.cpp
+++ tests/iterator.cpp
@@ -2,3 +2,23 @@
 
 #include "catch.hpp"
 #include "../include/iterator.hpp"
+
+#include <array>
+#include <list>
+#include <vector>
+
+TEST_CASE("zip", "[iterator]") {
+    std::vector<int> vec{1, 2, 3, 4};
+    std::array<char, 3> arr{'a', 'b', 'c'};
+    std::list<float> lst{1., 2., 3.};
+
+    for (auto [i, c, f] : extra::zip(vec, arr, lst)) {
+        i = 0;
+        c = 'a';
+        f = 3.14f;
+    }
+
+    REQUIRE(vec == std::vector<int>{1, 1, 1});
+    REQUIRE(arr == std::array<char, 3>{'a', 'a', 'a'});
+    REQUIRE(lst == std::list<float>{3.14f, 3.14f, 3.14f});
+}
